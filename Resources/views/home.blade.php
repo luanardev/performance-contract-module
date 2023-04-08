d15698228c69876d97401373ce55436bc77225b4
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:44:59 +0200

    Update home.blade.php

--- Resources/views/home.blade.php
+++ Resources/views/home.blade.php
@@ -1,9 +1,41 @@
 @extends('performancecontract::layouts.app')
 
 @section('content')
-    <h1>Hello World</h1>
 
-    <p>
-        This view is loaded from module: {!! config('performancecontract.name') !!}
-    </p>
+    <div class="container-fluid">
+
+        <div class="content-header">
+
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h4 class="m-0">Performance Contract</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('performance_contract.home') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Home</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <livewire:performancecontract::draft-plans
+                        :staff="$staff"
+                        :financialYear="$financialYear"
+                    />
+                    <livewire:performancecontract::recent-plans
+                        :staff="$staff"
+                        :financialYear="$financialYear"
+                    />
+                    <livewire:performancecontract::previous-plans
+                        :staff="$staff"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
