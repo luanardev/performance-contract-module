a08079791015ee691d1a5c12f066f7af58a67725
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:44:45 +0200

    Update PreviousPlans.php

--- Http/Livewire/PreviousPlans.php
+++ Http/Livewire/PreviousPlans.php
@@ -2,10 +2,30 @@
 
 namespace Lumis\PerformanceContract\Http\Livewire;
 
-use Livewire\Component;
+use Illuminate\Support\Collection;
+use Luanardev\LivewireUI\LivewireUI;
+use Lumis\PerformanceContract\Entities\StaffPlan;
+use Lumis\StaffManagement\Entities\Staff;
 
-class PreviousPlans extends Component
+class PreviousPlans extends LivewireUI
 {
+    /**
+     * @var Collection
+     */
+    public Collection $previousPlans;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->previousPlans = collect();
+    }
+
+    public function mount(Staff $staff)
+    {
+        $staffPlan = new StaffPlan($staff);
+        $this->previousPlans = $staffPlan->getArchivePlans();
+    }
+
     public function render()
     {
         return view('performancecontract::livewire.previous-plans');
