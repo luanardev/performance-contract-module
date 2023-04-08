ef418e3e5e41a113d8df3f801aeed36e78344172
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:45:16 +0200

    Update recent-plans.blade.php

--- Resources/views/livewire/recent-plans.blade.php
+++ Resources/views/livewire/recent-plans.blade.php
@@ -1,3 +1,56 @@
 <div>
-    <h3>The <code>RecentPlans</code> livewire component is loaded from the <code>PerformanceContract</code> module.</h3>
+    <div class="card card-outline card-success">
+        <div class="card-header">
+            <div class="card-title">My Recent Plans</div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    @if(count($recentPlans) > 0)
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <td>#</td>
+                                        <th>Session</th>
+                                        <th>Appraiser</th>
+                                        <th>Created</th>
+                                        <th>Status</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($recentPlans as $key => $plan)
+                                    <tr>
+                                        <td>{{++$key}}</td>
+                                        <td class="text-capitalize">
+                                            {{strtoupper($plan->financialYear->name)}}
+                                        </td>
+                                        <td>{{$plan->appraiser->fullname()}}</td>
+                                        <td>{{$plan->createdPeriod()}}</td>
+                                        <td>{!! $plan->statusBadge() !!}</td>
+                                        <td>
+                                            <a href="{{route('performance_contract.show', $plan)}}"
+                                               class="btn btn-sm btn-outline-primary">Open</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    @else
+                        <div class="callout callout-info py-2 mb-4">
+                            <p>
+                                No recent plans found.
+                            </p>
+                        </div>
+                    @endif
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
+
+
