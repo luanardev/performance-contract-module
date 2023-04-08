852c6a91c4231bc55a167494c19916cb14e448d8
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:44:54 +0200

    Update create.blade.php

--- Resources/views/create.blade.php
+++ Resources/views/create.blade.php
@@ -8,7 +8,7 @@
 
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h4 class="m-0">Create Performance Plan</h4>
+                    <h4 class="m-0">Create Performance Contract</h4>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
