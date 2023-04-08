0d202d46ee6cdda252fe82e15ba2276a80eb3712
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:45:05 +0200

    Update index.blade.php

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -8,7 +8,7 @@
 
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h4 class="m-0">Performance Contract</h4>
+                    <h4 class="m-0">My Performance Contract</h4>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -22,8 +22,13 @@
         <div class="content">
             <div class="row">
                 <div class="col-lg-12">
-                    <livewire:performancecontract::recent-plans />
-                    <livewire:performancecontract::previous-plans />
+                    <livewire:performancecontract::recent-plans
+                        :staff="$staff"
+                        :financialYear="$financialYear"
+                    />
+                    <livewire:performancecontract::previous-plans
+                        :staff="$staff"
+                    />
                 </div>
             </div>
         </div>
