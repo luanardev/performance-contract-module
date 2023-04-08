80064118d259749d1b2256bfe521b582b679e20c
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-08 02:44:49 +0200

    Update RecentPlans.php

--- Http/Livewire/RecentPlans.php
+++ Http/Livewire/RecentPlans.php
@@ -2,10 +2,30 @@
 
 namespace Lumis\PerformanceContract\Http\Livewire;
 
-use Livewire\Component;
+use Illuminate\Support\Collection;
+use Luanardev\LivewireUI\LivewireUI;
+use Lumis\Organization\Entities\FinancialYear;
+use Lumis\PerformanceContract\Entities\StaffPlan;
+use Lumis\StaffManagement\Entities\Staff;
 
-class RecentPlans extends Component
+class RecentPlans extends LivewireUI
 {
+    /**
+     * @var Collection
+     */
+    public Collection $recentPlans;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->recentPlans = collect();
+    }
+
+    public function mount(Staff $staff, FinancialYear $financialYear)
+    {
+        $staffPlan = new StaffPlan($staff, $financialYear);
+        $this->recentPlans = $staffPlan->getRecentPlans();
+    }
 
     public function render()
     {
