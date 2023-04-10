fc52e6a3810e97d7c79acce4d29c7833ae953f77
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:40:52 +0200

    Create rate-mid-year.blade.php

---
+++ Resources/views/livewire/rating/rate-mid-year.blade.php
@@ -0,0 +1,66 @@
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="bg-dark">
+                        <th colspan="4">
+                           {{$pillar->name}} ({{$this->getTotalWeight()}} %)
+                        </th>
+                        <th colspan="3">Mid Year Rating</th>
+                    </tr>
+                    <tr>
+                        <th scope="col" style="width: 20%">Objectives(Goals)</th>
+                        <th scope="col" style="width: 20%">Measures(KPIs)</th>
+                        <th scope="col" style="width: 20%">Targets</th>
+                        <th scope="col" style="width: 10%">Weight</th>
+                        <th scope="col" style="width: 10%">Rate</th>
+                        <th scope="col" style="width: 20%">Outcome</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($this->deliverables() as $deliverable)
+
+                        @foreach($deliverable->indicators as $index => $indicator)
+
+                            @if($index == 0)
+                                <tr>
+                                    <td class="font-weight-normal" rowspan="{{$deliverable->indicators->count()}}">
+                                        {{$deliverable->name}}
+                                        ({{$deliverable->totalWeight()}} %)
+                                    </td>
+
+                                    <td class="font-italic">{{$indicator->name}}</td>
+                                    <td class="font-italic">{{$indicator->target}}</td>
+                                    <td class="font-italic">{{$indicator->weight}} %
+                                    <td>
+                                        <input wire:model.lazy="ratings.{{$indicator->id}}.self_rate" class="form-control" />
+                                    </td>
+                                    <td>
+                                        <input wire:model.lazy="ratings.{{$indicator->id}}.outcome" class="form-control" />
+                                    </td>
+
+                                </tr>
+                            @else
+                                <tr>
+                                    <td class="font-italic">{{$indicator->name}}</td>
+                                    <td class="font-italic">{{$indicator->target}}</td>
+                                    <td class="font-italic">{{$indicator->weight}} %
+                                    <td>
+                                        <input wire:model.lazy="ratings.{{$indicator->id}}.self_rate" class="form-control" />
+                                    </td>
+                                    <td>
+                                        <input wire:model.lazy="ratings.{{$indicator->id}}.outcome" class="form-control" />
+                                    </td>
+                                </tr>
+                            @endif
+                        @endforeach
+
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
