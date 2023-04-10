445e890339c71043919fef9e1fd90e3ec49b607a
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:40:31 +0200

    Create recent-plans.blade.php

---
+++ Resources/views/livewire/planning/recent-plans.blade.php
@@ -0,0 +1,56 @@
+<div>
+    <div class="card card-outline card-success">
+        <div class="card-header">
+            <div class="card-title">My Recent Plans</div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    @if(count($recentPlans) > 0)
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <td>#</td>
+                                        <th>Session</th>
+                                        <th>Supervisor</th>
+                                        <th>Submitted</th>
+                                        <th>Status</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($recentPlans as $key => $plan)
+                                    <tr>
+                                        <td>{{++$key}}</td>
+                                        <td class="text-capitalize">
+                                            {{strtoupper($plan->financialYear->name)}}
+                                        </td>
+                                        <td>{{$plan->appraiser->fullname()}}</td>
+                                        <td>{{$plan->submittedPeriod()}}</td>
+                                        <td>{!! $plan->statusBadge() !!}</td>
+                                        <td>
+                                            <a href="{{route('performance_contract.show', $plan)}}"
+                                               class="btn btn-sm btn-outline-primary">Open</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    @else
+                        <div class="callout callout-info py-2 mb-4">
+                            <p>
+                                No recent plans found.
+                            </p>
+                        </div>
+                    @endif
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
