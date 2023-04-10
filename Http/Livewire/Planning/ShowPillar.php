003eb21518802d26e1e8a4cdb00e8f966993366d
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:39:03 +0200

    Create ShowPillar.php

---
+++ Http/Livewire/Planning/ShowPillar.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lumis\PerformanceContract\Http\Livewire\Planning;
+
+use Illuminate\Support\Collection;
+use Luanardev\LivewireUI\LivewireUI;
+use Lumis\PerformanceContract\Entities\Pillar;
+use Lumis\PerformanceContract\Entities\Plan;
+
+class ShowPillar extends LivewireUI
+{
+    public Plan $plan;
+    public Pillar $pillar;
+
+    public function mount(Plan $plan, Pillar $pillar)
+    {
+        $this->plan = $plan;
+        $this->pillar = $pillar;
+
+    }
+
+    public function deliverables(): Collection
+    {
+        return $this->plan->getPillarDeliverables($this->pillar);
+    }
+
+    public function getTotalWeight()
+    {
+        return $this->plan->getPillarWeight($this->pillar);
+    }
+
+    public function hasDeliverables(): bool
+    {
+        return (bool) $this->plan->deliverables->count();
+
+    }
+
+    public function render()
+    {
+        return view('performancecontract::livewire.planning.show-pillar');
+    }
+}
