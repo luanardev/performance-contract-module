92aa44261a615d6b413b0b3d282802211cdd548a
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:41:25 +0200

    Create endyear-rating.blade.php

---
+++ Resources/views/rating/endyear-rating.blade.php
@@ -0,0 +1,35 @@
+@extends('performancecontract::layouts.app')
+
+@section('content')
+
+    <div class="container-fluid">
+
+        <div class="content-header">
+
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h4 class="m-0">End Year Rating
+                        <span class="text-muted font-weight-light">
+                            ({{strtoupper($plan->financialYear->name)}})
+                        </span>
+                    </h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('performance_contract.home') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Edit</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <livewire:performancecontract::rating.end-year-rating :plan="$plan" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
