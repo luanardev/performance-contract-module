15ebab1efd04f2c0b2295328f04387021d95878f
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:40:55 +0200

    Create rating-nav.blade.php

---
+++ Resources/views/livewire/rating/rating-nav.blade.php
@@ -0,0 +1,17 @@
+<nav class="mb-4">
+    <ul class="nav nav-pills flex-sm-row"  role="menu">
+
+        <li class="nav-item">
+            <a href="{{route('performance_contract.home')}}" class="nav-link">
+                <i class="fa fa-home"></i> Home
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="{{route('performance_contract.show', $plan)}}" class="nav-link">
+                <i class="fa fa-eye"></i> View
+            </a>
+        </li>
+
+    </ul>
+</nav>
