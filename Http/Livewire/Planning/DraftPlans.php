c1a266b9bde6c0c8545be2565655f3fbf6d35cae
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-10 07:38:47 +0200

    Create DraftPlans.php

---
+++ Http/Livewire/Planning/DraftPlans.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lumis\PerformanceContract\Http\Livewire\Planning;
+
+use Illuminate\Support\Collection;
+use Luanardev\LivewireUI\LivewireUI;
+use Lumis\Organization\Entities\FinancialYear;
+use Lumis\PerformanceContract\Entities\Plan;
+use Lumis\PerformanceContract\Entities\StaffPlan;
+use Lumis\StaffManagement\Entities\Staff;
+
+class DraftPlans extends LivewireUI
+{
+    /**
+     * @var Collection
+     */
+    public Collection $draftPlans;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->draftPlans = collect();
+    }
+
+    public function mount(Staff $staff, FinancialYear $financialYear)
+    {
+        $staffPlan = new StaffPlan($staff, $financialYear);
+        $this->draftPlans = $staffPlan->getRecentDrafts();
+    }
+
+    public function delete(Plan $plan)
+    {
+        $plan->delete();
+        $this->toastr('Draft deleted');
+        $this->redirect(route('performance_contract.home'));
+    }
+
+    public function render()
+    {
+        return view('performancecontract::livewire.planning.draft-plans');
+    }
+}
