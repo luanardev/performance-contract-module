a9b39c1318a8506b0f5dc92e87f74a695518ea8b
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:38:47 +0200

    Create reuse.blade.php

---
+++ Resources/views/planning/reuse.blade.php
@@ -0,0 +1,34 @@
+@extends('performancecontract::layouts.app')
+
+@section('content')
+
+    <div class="container-fluid">
+
+        <div class="content-header">
+
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h4 class="m-0">Reuse Performance Plan</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('performance_contract.home') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Reuse</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <livewire:performancecontract::planning.reuse-plan
+                        :plan="$plan"
+                    />
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
