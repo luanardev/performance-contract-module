91eb246bc290b0ee3fdc8118741a2b557fa77549
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:35:33 +0200

    Create MidYearRating.php

---
+++ Http/Livewire/Appraisal/MidYearRating.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lumis\PerformanceContract\Http\Livewire\Appraisal;
+
+use Illuminate\Support\Collection;
+use Luanardev\LivewireUI\LivewireUI;
+use Lumis\PerformanceContract\Entities\Pillar;
+use Lumis\PerformanceContract\Entities\Plan;
+
+class MidYearRating extends LivewireUI
+{
+    public Plan $plan;
+    public Collection $pillars;
+
+    public function mount(Plan $plan)
+    {
+        $this->plan = $plan;
+        $this->pillars = Pillar::all();
+    }
+
+    public function render()
+    {
+        return view('performancecontract::livewire.appraisal.mid-year-rating');
+    }
+}
