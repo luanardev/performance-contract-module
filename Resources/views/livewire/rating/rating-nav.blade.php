2d93cd18c7b8f4d704fbc059f2a43e850afb4535
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:38:34 +0200

    Update rating-nav.blade.php

--- Resources/views/livewire/rating/rating-nav.blade.php
+++ Resources/views/livewire/rating/rating-nav.blade.php
@@ -8,7 +8,7 @@
         </li>
 
         <li class="nav-item">
-            <a href="{{route('performance_contract.show', $plan)}}" class="nav-link">
+            <a href="{{route('performance_contract.plan.show', $plan)}}" class="nav-link">
                 <i class="fa fa-eye"></i> View
             </a>
         </li>
