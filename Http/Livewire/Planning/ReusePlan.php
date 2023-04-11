a3c968706ca4d27c35b191435840d1c679fc286a
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:36:06 +0200

    Create ReusePlan.php

---
+++ Http/Livewire/Planning/ReusePlan.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Lumis\PerformanceContract\Http\Livewire\Planning;
+
+use Lumis\PerformanceContract\Entities\Deliverable;
+use Lumis\PerformanceContract\Entities\Indicator;
+use Lumis\PerformanceContract\Entities\Plan;
+use Lumis\StaffManagement\Entities\StaffProfile;
+
+class ReusePlan extends CopyPlan
+{
+
+    public function save()
+    {
+        $this->validate();
+
+        $staff = StaffProfile::get();
+
+        $submitted = Plan::submitted(
+            $staff,
+            $this->appraiser,
+            $this->financialYear
+        );
+
+        if($submitted){
+            $this->toastrError('Performance contract exists');
+        }
+        else{
+            $newPlan = new Plan();
+            $newPlan->staff()->associate($staff);
+            $newPlan->financialYear()->associate($this->financialYear);
+            $newPlan->appraiser()->associate($this->appraiser);
+            $newPlan->save();
+
+            foreach($this->plan->deliverables as $deliverable) {
+
+                $newDeliverable = new Deliverable();
+                $newDeliverable->name = $deliverable->name;
+                $newDeliverable->plan()->associate($newPlan);
+                $newDeliverable->pillar()->associate($deliverable->pillar);
+                $newDeliverable->save();
+
+                foreach($deliverable->indicators as $indicator){
+                    $newIndicator = new Indicator();
+                    $newIndicator->name = $indicator->name;
+                    $newIndicator->target = $indicator->target;
+                    $newIndicator->weight = $indicator->weight;
+                    $newIndicator->deliverable()->associate($newDeliverable);
+                    $newIndicator->save();
+                }
+            }
+
+            $this->toastrSuccess('Performance contract created');
+            $this->redirectRoute('performance_contract.plan.edit', [$newPlan]);
+        }
+
+    }
+
+}
