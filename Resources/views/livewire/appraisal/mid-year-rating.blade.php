486470d55c37ff96bef596bcf172028de87f68dc
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:37:44 +0200

    Create mid-year-rating.blade.php

---
+++ Resources/views/livewire/appraisal/mid-year-rating.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <div class="row">
+        <div class="col-lg-12">
+            <nav class="mb-4">
+                <ul class="nav nav-pills flex-sm-row"  role="menu">
+                    <li class="nav-item">
+                        <a href="{{route('performance_contract.appraisal.show', $plan)}}" class="nav-link">
+                            <i class="fa fa fa-arrow-left"></i> Back
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <livewire:performancecontract::appraisal.header
+                :plan="$plan"
+            />
+        </div>
+    </div>
+    @foreach($pillars as $key => $pillar)
+        <livewire:performancecontract::appraisal.rate-mid-year
+            :plan=$plan
+            :pillar=$pillar
+            :wire:key="'rate-mid-year-'.$key.'-'.$pillar->id"
+        />
+    @endforeach
+</div>
