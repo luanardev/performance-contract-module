b3554f368a54ea8cc6dacac2cb09fba0dc823a48
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-11 22:36:58 +0200

    Create PlanObserver.php

---
+++ Observers/PlanObserver.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Lumis\PerformanceContract\Observers;
+
+use Lumis\PerformanceContract\Entities\Plan;
+
+class PlanObserver
+{
+
+
+    /**
+     * Handle the "created" event.
+     *
+     * @param Plan $plan
+     * @return void
+     */
+    public function created(Plan $plan): void
+    {
+
+    }
+
+    /**
+     * Handle the "updated" event.
+     *
+     * @param Plan $plan
+     * @return void
+     */
+    public function updated(Plan $plan): void
+    {
+
+
+    }
+
+    /**
+     * Handle the "deleted" event.
+     *
+     * @param Plan $plan
+     * @return void
+     */
+    public function deleted(Plan $plan): void
+    {
+
+    }
+
+
+}
