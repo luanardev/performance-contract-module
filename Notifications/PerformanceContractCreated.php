5168f4238d8f2d2ff395c68cd011f257b28a38bc
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-12 11:42:50 +0200

    Create PerformanceContractCreated.php

---
+++ Notifications/PerformanceContractCreated.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Lumis\PerformanceContract\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Lumis\PerformanceContract\Entities\Plan;
+
+class PerformanceContractCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * @var Plan
+     */
+    public Plan $plan;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Plan $plan)
+    {
+        $this->plan = $plan;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->markdown('performancecontract::emails.performance_contract_created', [
+                'plan' => $this->plan
+            ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
