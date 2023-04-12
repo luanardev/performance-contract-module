1a98776ba36db30fdd1935079c6b23a232f9913e
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-12 11:42:48 +0200

    Update HandlePlanSubmitted.php

--- Listeners/HandlePlanSubmitted.php
+++ Listeners/HandlePlanSubmitted.php
@@ -2,10 +2,13 @@
 
 namespace Lumis\PerformanceContract\Listeners;
 
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Notification;
+use Lumis\PerformanceContract\Events\PlanSubmitted;
+use Lumis\PerformanceContract\Notifications\PerformanceContractSubmission;
+use Lumis\PerformanceContract\Notifications\PerformanceContractSubmitted;
 
-class HandlePlanSubmitted
+class HandlePlanSubmitted implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -20,11 +23,22 @@ class HandlePlanSubmitted
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  PlanSubmitted  $event
      * @return void
      */
-    public function handle($event)
+    public function handle(PlanSubmitted $event): void
     {
-        //
+        $plan = $event->plan;
+        $staff = $event->plan->staff;
+        $appraiser = $event->plan->appraiser;
+
+        // notify the staff who submitted the plan
+        $staffNotification = new PerformanceContractSubmitted($plan);
+        Notification::send($staff, $staffNotification);
+
+        // notify appraiser about the submitted plan
+        $appraiserNotification = new PerformanceContractSubmission($plan);
+        Notification::send($appraiser, $appraiserNotification);
+
     }
 }
