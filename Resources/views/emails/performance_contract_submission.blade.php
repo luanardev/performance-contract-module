2bede1292f6f737cf6f4b9e20ce449813b0c8b5a
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-12 11:43:14 +0200

    Create performance_contract_submission.blade.php

---
+++ Resources/views/emails/performance_contract_submission.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+<h2>Dear {{$plan->appraiser->fullname()}}</h2>
+
+<p>You are hereby notified that <strong>{{$plan->staff->fullname()}}</strong> who works as <strong>{{$plan->staff->employment->getPosition()}}</strong> has submitted {{$plan->staff->possessivePronoun()}} Performance Contract for <strong>{{$plan->financialYear->name}}</strong> Financial Year.</p>
+<p>Please <a href="{{route('login')}}">login</a> to your staff account to review {{$plan->staff->genderism()}} performance contract.</p>
+
+{{ config('app.name') }}
+@endcomponent
