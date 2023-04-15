4e0ff48d478553f396d6124911ae8e289e45cbbc
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 17:47:55 +0200

    Update CreatePlan.php

--- Http/Livewire/Planning/CreatePlan.php
+++ Http/Livewire/Planning/CreatePlan.php
@@ -10,34 +10,53 @@ use Lumis\StaffManagement\Entities\StaffProfile;
 
 class CreatePlan extends LivewireUI
 {
-    public mixed $financialYear;
-    public mixed $appraiser;
     public mixed $financialYears;
     public mixed $staffMembers;
+    public mixed $searchAppraiser;
+    public mixed $selectedAppraiser;
+    public mixed $selectedYear;
+    public mixed $showDropdown = true;
 
     public function __construct()
     {
         parent::__construct();
-        $this->financialYear = null;
-        $this->appraiser = null;
-        $this->financialYears = $this->financialYears();
-        $this->staffMembers = Staff::all();
+        $this->selectedYear = null;
+        $this->selectedAppraiser = null;
+        $this->staffMembers = collect();
+        $this->searchAppraiser = null;
+        $this->financialYears = FinancialYear::latest()->limit(5)->get();
     }
 
-    public function financialYears()
+    public function updatedSearchAppraiser($value)
     {
-        return FinancialYear::latest()->limit(5)->get();
+        $this->toggleDropdown();
+        if(!empty($value)){
+            $this->staffMembers = Staff::search($value)->get();
+        }
+    }
+
+    public function selectAppraiser(Staff $staff)
+    {
+        $this->selectedAppraiser = $staff;
+        $this->searchAppraiser = $staff->fullname()." ({$staff->employment->getPosition()})";
+        $this->toggleDropdown();
+    }
+
+    private function toggleDropdown()
+    {
+        $this->showDropdown = !$this->showDropdown;
     }
 
     public function save()
     {
         $this->validate();
+
         $staff = StaffProfile::get();
 
         $submitted = Plan::submitted(
             $staff,
-            $this->appraiser,
-            $this->financialYear
+            $this->selectedAppraiser,
+            $this->selectedYear
         );
 
         if($submitted){
@@ -46,12 +65,12 @@ class CreatePlan extends LivewireUI
         else{
             $plan = new Plan();
             $plan->staff()->associate($staff);
-            $plan->financialYear()->associate($this->financialYear);
-            $plan->appraiser()->associate($this->appraiser);
+            $plan->financialYear()->associate($this->selectedYear);
+            $plan->appraiser()->associate($this->selectedAppraiser);
             $plan->setDraft();
             $plan->save();
             $this->toastrSuccess('Performance contract created');
-            $this->redirectRoute('performance_contract.plan.edit', [$plan]);
+            $this->redirectRoute('performance_contract.plan.edit', $plan);
         }
 
     }
@@ -59,8 +78,8 @@ class CreatePlan extends LivewireUI
     public function rules()
     {
         return [
-          'financialYear' => 'required',
-          'appraiser' => 'required'
+          'selectedYear' => 'required',
+          'searchAppraiser' => 'required'
         ];
     }
 
