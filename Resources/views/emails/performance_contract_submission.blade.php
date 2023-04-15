fa0724f922a1f6daafb3f7bcc13bcc077fb84e4c
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-04-15 17:48:11 +0200

    Update performance_contract_submission.blade.php

--- Resources/views/emails/performance_contract_submission.blade.php
+++ Resources/views/emails/performance_contract_submission.blade.php
@@ -2,7 +2,7 @@
 <h2>Dear {{$plan->appraiser->fullname()}}</h2>
 
 <p>You are hereby notified that <strong>{{$plan->staff->fullname()}}</strong> who works as <strong>{{$plan->staff->employment->getPosition()}}</strong> has submitted {{$plan->staff->possessivePronoun()}} Performance Contract for <strong>{{$plan->financialYear->name}}</strong> Financial Year.</p>
-<p>Please <a href="{{route('login')}}">login</a> to your staff account to review {{$plan->staff->genderism()}} performance contract.</p>
+<p>Please <a href="{{route('login')}}">login</a> to your staff account to review {{$plan->staff->possessivePronoun()}} performance contract.</p>
 
 {{ config('app.name') }}
 @endcomponent
